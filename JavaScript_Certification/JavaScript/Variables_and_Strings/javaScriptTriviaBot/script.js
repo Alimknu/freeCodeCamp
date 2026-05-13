207b3c0288327b49b921f9738600cdbd168d9490
Ali Mirzakhalili <alimknu@gmail.com>
2026-05-12 23:47:12 -0400

    Variables and Strings complete

---
+++ JavaScript_Certification/JavaScript/Variables_and_Strings/javaScriptTriviaBot/script.js
@@ -0,0 +1,30 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia Bot";
+let botLocation = "the universe";
+let favoriteLanguage = "C++";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  "I enjoy coding in " + favoriteLanguage + " because of its performance.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage + " allows fine-grained memory control and high speed.";
+console.log(codingFact);
+
+codingFact =
+  "Many performance-critical games and engines are written in " +
+  favoriteLanguage +
+  ".";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
---
+++ JavaScript_Certification/JavaScript/Variables_and_Strings/sentenceMaker/script.js
@@ -0,0 +1,21 @@
+let adjective = "magical";
+let noun = "cat";
+let verb = "sleeping";
+let place = "rainforest";
+let adjective2 = "sparkly";
+let noun2 = "fish";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+adjective = "fierce";
+noun = "dragon";
+verb = "angry";
+place = "castle";
+adjective2 = "scaly";
+noun2 = "sheep";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
