c0926712ac6ba99a7f477003da8e494f03d93691
Ali Mirzakhalili <alimknu@gmail.com>
2026-05-29 00:20:11 -0400

    Classes Complete

---
+++ JavaScript_Certification/JavaScript/Classes/projectIdeaBoard/script.js
@@ -0,0 +1,52 @@
+const projectStatus = {
+  PENDING: { description: "Pending Execution" },
+  SUCCESS: { description: "Executed Successfully" },
+  FAILURE: { description: "Execution Failed" },
+};
+
+class ProjectIdea {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.status = projectStatus.PENDING;
+  }
+
+  updateProjectStatus(newStatus) {
+    this.status = newStatus;
+  }
+}
+
+class ProjectIdeaBoard {
+  constructor(title) {
+    this.title = title;
+    this.ideas = [];
+  }
+
+  pin(idea) {
+    this.ideas.push(idea);
+  }
+
+  unpin(idea) {
+    const index = this.ideas.indexOf(idea);
+
+    if (index == -1) {
+      return;
+    }
+
+    this.ideas.splice(index, 1);
+  }
+
+  count() {
+    return this.ideas.length;
+  }
+
+  formatToString() {
+    let res = `${this.title} has ${this.count()} idea(s)\n`;
+
+    this.ideas.forEach((idea) => {
+      res += `${idea.title} (${idea.status.description}) - ${idea.description}\n`;
+    });
+
+    return res;
+  }
+}
